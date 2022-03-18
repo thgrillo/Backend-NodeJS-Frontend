2b2bec9e5e8bbe069409bfbc0a023cec6957e56a
Thiago Grillo <thiago_grillo@hotmail.com>
2022-03-18 04:50:44 -0300

    update

--- src/route.js
+++ src/route.js
@@ -3,5 +3,6 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render('index'))
+route.get('/blog', (req, res) => res.render('blog-single'))
 
 module.exports = route
